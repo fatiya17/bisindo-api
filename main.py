49ae59c8efa7986dc01c7a2a7a48b43eb6d086d1
fatiya17 <fatiyalabibah17@gmail.com>
2026-08-23 16:27:53 +0700

    feat: update API for Render deployment

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
+import os
+import urllib.request
 from fastapi import FastAPI, File, UploadFile
 from ultralytics import YOLO
 from PIL import Image
 import io
 
+# =========================================================================
+URL_MODEL_BISINDO = "https://huggingface.co/elfrumoasa/bisindo-yolo/blob/main/best-bisindo.pt"
+URL_MODEL_KESEHARIAN = "https://huggingface.co/elfrumoasa/bisindo-yolo/blob/main/best-keseharian.pt"
+# =========================================================================
+
+def download_model(url, filename):
+    # Hanya download jika file belum ada
+    if not os.path.exists(filename):
+        print(f"Downloading {filename} from Hugging Face...")
+        try:
+            urllib.request.urlretrieve(url, filename)
+            print(f"✅ Downloaded {filename} successfully!")
+        except Exception as e:
+            print(f"❌ Error downloading {filename}: {e}")
+
+# Proses download dijalankan sebelum API menyala
+download_model(URL_MODEL_BISINDO, "best-bisindo.pt")
+download_model(URL_MODEL_KESEHARIAN, "best-keseharian.pt")
+
 app = FastAPI()
 
 # Load model hasil fine-tuning
-model = YOLO("best.pt")
-
+model_bisindo = YOLO("best-bisindo.pt")
+model_keseharian = YOLO("best-keseharian.pt")
 
 @app.get("/")
 def root():
@@ -25,7 +46,7 @@ async def predict(file: UploadFile = File(...)):
         io.BytesIO(image_bytes)
     ).convert("RGB")
 
-    results = model.predict(
+    results = model_bisindo.predict(
         source=image,
         conf=0.5,
         verbose=False
@@ -40,7 +61,38 @@ async def predict(file: UploadFile = File(...)):
         confidence = float(box.conf[0])
 
         predictions.append({
-            "class": model.names[class_id],
+            "class": model_bisindo.names[class_id],
+            "confidence": round(confidence, 4)
+        })
+
+    return {
+        "predictions": predictions
+    }
+
+@app.post("/predict-keseharian")
+async def predict_keseharian(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+
+    image = Image.open(
+        io.BytesIO(image_bytes)
+    ).convert("RGB")
+
+    results = model_keseharian.predict(
+        source=image,
+        conf=0.5,
+        verbose=False
+    )
+
+    result = results[0]
+
+    predictions = []
+
+    for box in result.boxes:
+        class_id = int(box.cls[0])
+        confidence = float(box.conf[0])
+
+        predictions.append({
+            "class": model_keseharian.names[class_id],
             "confidence": round(confidence, 4)
         })
 
